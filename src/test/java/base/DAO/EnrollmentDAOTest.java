55b5cdf737e9f2685a8aac7d28055b6d62c3067a
MalenaLucero <luceromalena@hotmail.com>
2021-01-26 20:21:26 -0300

    valid enrollment test

--- src/test/java/base/DAO/EnrollmentDAOTest.java
+++ src/test/java/base/DAO/EnrollmentDAOTest.java
@@ -12,12 +12,25 @@ import base.model.Enrollment;
 
 public class EnrollmentDAOTest {
 	@Test
-	public void insertEnrollmentTest() throws ClassNotFoundException, SQLException {
+	public void insertInvalidEnrollmentTest() throws ClassNotFoundException, SQLException {
 		Connection connection = AdminDB.obtenerConexion();
 		List<Enrollment> totalBefore = EnrollmentDAO.getAll(connection);
 		Enrollment enrollment = new Enrollment(4, 1);
 		EnrollmentController.insert(connection, enrollment);
 		List<Enrollment> totalAfter = EnrollmentDAO.getAll(connection);
 		Assert.assertEquals(totalBefore.size(), totalAfter.size());
+	}
+	
+	@Test
+	public void insertValidEnrollmentTest() throws ClassNotFoundException, SQLException {
+		Connection connection = AdminDB.obtenerConexion();
+		List<Enrollment> totalBefore = EnrollmentDAO.getAll(connection);
+		Enrollment enrollment = new Enrollment(1, 3);
+		EnrollmentController.insert(connection, enrollment);
+		List<Enrollment> totalAfter = EnrollmentDAO.getAll(connection);
+		Assert.assertEquals(totalBefore.size() + 1, totalAfter.size());
+		Enrollment findEnrollment = EnrollmentDAO.findByCourseAndStudent(connection, 1, 3);
+		int res = EnrollmentDAO.delete(findEnrollment.getId(), connection);
+		Assert.assertTrue(res == 1);
 	}
 }
